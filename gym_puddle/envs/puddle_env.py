3b39b53921306ba7c3647640c5479b9b085f4004
Ehsan <imani@ualberta.ca>
2018-01-08 18:53:08 -0700

    Rendering. Still not quite right.

--- gym_puddle/envs/puddle_env.py
+++ gym_puddle/envs/puddle_env.py
@@ -2,6 +2,8 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
+import copy
+from gym.envs.classic_control import rendering
 
 class PuddleEnv(gym.Env):
     metadata = {'render.modes': ['human']}
@@ -25,6 +27,7 @@ class PuddleEnv(gym.Env):
             self.actions[i][i//2] = thrust * (i%2 * 2 - 1)
 
         self._seed()
+        self.viewer = None
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
@@ -33,22 +36,30 @@ class PuddleEnv(gym.Env):
     def _step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
 
+        self.last_pos = copy.copy(self.pos)
+
         self.pos += self.actions[action] + self.np_random.uniform(low=-self.noise, high=self.noise, size=(2,))
         self.pos = np.clip(self.pos, 0.0, 1.0)
 
-        reward = -1.
-        for cen, wid in zip(self.puddle_center, self.puddle_width):
-            reward -= 2. * self._gaussian1d(self.pos[0], cen[0], wid[0]) * \
-                self._gaussian1d(self.pos[1], cen[1], wid[1])
+        reward = self._get_reward(self.pos)
 
         done = np.linalg.norm((self.pos - self.goal), ord=1) < self.goal_threshold
 
         return self.pos, reward, done, {}
 
+    def _get_reward(self, pos):
+        reward = -1.
+        for cen, wid in zip(self.puddle_center, self.puddle_width):
+            reward -= 2. * self._gaussian1d(pos[0], cen[0], wid[0]) * \
+                self._gaussian1d(pos[1], cen[1], wid[1])
+
+        return reward
+
     def _gaussian1d(self, p, mu, sig):
         return np.exp(-((p - mu)**2)/(2.*sig**2)) / (sig*np.sqrt(2.*np.pi))
 
     def _reset(self):
+        self.last_pos = None
         if self.start is None:
             self.pos = self.observation_space.sample()
         else:
@@ -56,4 +67,72 @@ class PuddleEnv(gym.Env):
         return self.pos
 
     def _render(self, mode='human', close=False):
-        return
+        if close:
+            if self.viewer is not None:
+                self.viewer.close()
+                self.viewer = None
+            return
+
+        screen_width = 600
+        screen_height = 400
+
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(screen_width, screen_height)
+
+            import pyglet
+            img_width = 100
+            img_height = 100
+            fformat = 'RGB'
+            pixels = np.zeros((img_width, img_height, len(fformat)))
+            for i in range(img_width):
+                for j in range(img_height):
+                    x = float(i)/img_width
+                    y = float(j)/img_height
+                    pixels[i,j,:] = self._get_reward(np.array([x,y]))
+
+            pixels -= pixels.min()
+            pixels *= 255./pixels.max()
+            pixels = np.floor(pixels)
+
+            img = pyglet.image.create(img_width, img_height)
+            img.format = fformat
+            data=[chr(int(pixel)) for pixel in pixels.flatten()]
+
+            img.set_data(fformat, img_width * len(fformat), ''.join(data))
+            bg_image = Image(img, screen_width, screen_height)
+            bg_image.set_color(1.0,1.0,1.0)
+
+            self.viewer.add_geom(bg_image)
+
+
+            l, r = self.pos[0]*screen_width-3, self.pos[0]*screen_width+3
+            t, b = self.pos[1]*screen_height+3, self.pos[1]*screen_height-3
+            agent_polygon = rendering.FilledPolygon([(l,b), (l,t), (r,t), (r,b)])
+            agent_polygon.set_color(0.0,1.0,0.0)
+            self.agenttrans = rendering.Transform()
+            agent_polygon.add_attr(self.agenttrans)
+            self.viewer.add_geom(agent_polygon)
+
+        self.agenttrans.set_translation(self.pos[0]*screen_width, self.pos[1]*screen_height)
+        # Uncomment these if you want to see the trajectory.
+        # Warning: It will slow down the program.
+
+        # if self.last_pos is not None:
+        #     scale = np.array((screen_width, screen_height))
+        #     movement = rendering.Line(start=self.last_pos*scale, end=self.pos*scale)
+        #     movement.set_color(0.0,0.0,0.7)
+        #     self.viewer.add_geom(movement)
+
+
+        return self.viewer.render(return_rgb_array = mode=='rgb_array')
+
+
+class Image(rendering.Geom):
+    def __init__(self, img, width, height):
+        rendering.Geom.__init__(self)
+        self.width = width
+        self.height = height
+        self.img = img
+        self.flip = False
+    def render1(self):
+        self.img.blit(0, 0, width=self.width, height=self.height)
